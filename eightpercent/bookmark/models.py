cc6aee7329ca2e45512684efa56e6a38fbbb4d58
Sungeheon <elon4856@gmail.com>
2021-11-16 13:29:51 +0900

    remove: bookmark app 삭제

--- eightpercent/bookmark/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Bookmark(models.Model):
-    title = models.CharField('Title', max_length=100)
-    asda  = models.CharField('asd', max_length=100, default="Adf")
